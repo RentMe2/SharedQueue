98c2b809efc8f23cc3324df8913f50b678b3a5bc
moeed <moeedulla12@gmail.com>
2021-07-28 03:53:30 -0700

    added support for test module

--- main.cpp
+++ main.cpp
@@ -2,46 +2,19 @@
 #include <iostream>
 #include <thread>
 using namespace shareQueue;
-void push(shared_queue &q){
-    std::string temp {"abc"};
-    size_t total_iter = 100;
-    while(total_iter>0){
-        q.push(temp);
-        total_iter--;
-    }
-}
-void pop(shared_queue &q){
-    std::string temp {"abc"};
-    size_t total_iter = 100;
-    while(total_iter>0){
-        std::string ret =q.pop_b();
-        if( !ret.empty() and ret!= temp ){
-            std::cout<<("STRING MISMATCH\n\n");
-            return;
-        }
-        total_iter--;
-    }
-}
 int main(){
     size_t maxSize = 100;
     shared_queue q(maxSize);
-    std::vector<std::thread> thrds;
+    std::string sample = "sample";
+    q.push(sample);
+    auto ret = q.pop();
 
-    thrds.emplace_back(std::thread(push, std::ref(q)));
-    thrds.emplace_back(std::thread(push, std::ref(q)));
-    thrds.emplace_back(std::thread(pop, std::ref(q)));
-    thrds.emplace_back(std::thread(pop, std::ref(q)));
-
-    for(auto&thrd:thrds)
-        thrd.join();
-
-    if(q.size() != 0){
-        std::cout<<("INCORRECT SIZE\n");
-        std::cout<<("SIZE : " )<<q.size() <<"\n";
-    }else{
-        std::cout<<("TEST CASE PASSED\n");
+    if(ret.empty()){
+        std::cout<<"ERR: Queue is Empty!\n";
     }
-
+    else{
+        std::cout<<"Queue Element : " << ret<<"\n";
+    }
 
     return 0;
 }
